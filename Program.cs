94c1da95511144f5ad620414dd3fe95ddb777aab
santiago pabon <santiago.pabon72@gmail.com>
2025-01-23 22:03:40 -0700

    Task 1 completed

--- Program.cs
+++ Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int lowNumber;
+            int highNumber;
+            int Differance;
+            /* prompt user to enter low number*/
+            Console.Write("Enter a low number: ");
+            lowNumber = Convert.ToInt32(Console.ReadLine());
+
+            /*Prompt user to enter high number */
+            Console.WriteLine();
+            Console.Write("Enter a high number: ");
+            highNumber = Convert.ToInt32(Console.ReadLine());
+
+            Differance = highNumber - lowNumber;
+            Console.WriteLine($"{Differance} is the differance between the low and high number");
         }
     }
 }
